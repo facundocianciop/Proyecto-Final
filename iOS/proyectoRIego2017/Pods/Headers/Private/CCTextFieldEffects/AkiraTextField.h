e7e0b9a619696d15021b62720a075382214c3f83
Facundo Palma <fpalma2k@gmail.com>
2017-10-09 22:13:44 -0300

    delete ios

--- iOS/proyectoRIego2017/Pods/Headers/Private/CCTextFieldEffects/AkiraTextField.h
+++
@@ -1,1 +0,0 @@
-../../../CCTextFieldEffects/CCTextFieldEffects/AkiraTextField.h
--- iOS/proyectoRIego2017/Pods/Target Support Files/Pods-proyectoRiego2017UITests/Pods-proyectoRiego2017UITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_proyectoRiego2017UITests : NSObject
-@end
-@implementation PodsDummy_Pods_proyectoRiego2017UITests
-@end
